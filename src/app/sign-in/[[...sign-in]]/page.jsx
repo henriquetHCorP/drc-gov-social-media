dee3a3c54e50f084add50cfab21ba79ec9b9ef4a
henriquet kapema <henriquetkapema@gmail.com>
2024-11-25 15:39:27 +0100

    fix &apos;

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,6 +1,9 @@
+"use client"
 import { SignIn } from '@clerk/nextjs'
 
+
 export default function SignInPage() {
+  
   return (
     <div className="flex items-center justify-center p-3">
         <SignIn />
