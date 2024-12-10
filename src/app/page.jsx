be40cb53760f0d67256749dbb34257f17fe5b4a6
henriquet kapema <henriquetkapema@gmail.com>
2024-12-10 16:23:07 +0100

    Complete the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Link from 'next/link'; 
+import CallToAction from './Components/CallToAction'; 
+import PostCard from './Components/PostCard';
+import RecentPosts from './Components/RecentPosts';
 
-export default function Home() {
-  return (
-    <h1 className="text-red-500">
-      Home
-    </h1>
-  )
+export default async function Home() {
+  let posts = null; 
+   try {
+    const result = await fetch(process.env.URL + '/api/post/get', {
+      method: 'POST', 
+      body:JSON.stringify({limit:9, order:'desc'}), 
+      cache:'no-store', 
+    }); 
+
+    const data = await result.json(); 
+    posts = data.posts; 
+   } catch(error) {
+     console.log('Error getting posts:', error); 
+   }
+
+   return (
+    <div className=" flex flex-col justify-center items-center">
+    <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold lg:text-6xl">Welcome to the DRC Gov Social Media 2</h1>
+       <p className="text-gray-500 text-sm sm:text-base">
+             Discover a variety of articles and tutorials on topics such as web development, software engineering, and programming languages, all brought to you through a blog built with Next.js and{" "}
+             <a
+               href="https://go.clerk.com/fgJHKlt"
+               className="text-teal-500 hover:underline"
+               target='_blank'
+             >
+                 Clerk
+             </a>
+             .
+       </p>
+       <Link
+         href='/search'
+         className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+       >
+        View all posts
+       </Link>
+    </div>
+    <div className="p-3 bg-amber-100 dark:bg-slate-700">
+      <CallToAction /> 
+    </div>
+    <div className="p-3 flex flex-col gap-8 py07">
+      <RecentPosts limit={9} />
+      <Link
+        href={'/search?category=null'}
+        className="text-lg text-teal-500 hover:underline text-center"
+      >
+      View all posts
+      </Link>
+      </div>
+    </div>
+   ); 
 }
